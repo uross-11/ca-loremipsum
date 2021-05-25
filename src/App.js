f4b00e47ff49187ba6eabd65e7926d149208cc52
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-05-25 14:24:31 +0200

    Add layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
+const [count, setCount] = useState(0);
+const [text, setText] = useState([]);  
   
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log('hello world');
+}
+
+return (
+  <section className='section-center'>
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>paragraphs:</label>
+      <input
+        type='number'
+        name='amount'
+        id='amount'
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>generate</button>
+    </form>
+    <article className='lorem-text'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, eveniet.</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, eius!</p>
+    </article>
+  </section>
+  )
 }
 
 export default App;
